96cac066bf706974006ee0329b7aae0cbbb776f9
Davi <125833857+Davilsantos28@users.noreply.github.com>
2024-03-21 15:40:16 -0300

    primeiro commit

--- cartas.py
+++ cartas.py
@@ -1,9 +1,9 @@
 import random
 
 class Carta:
-    def _init_(self, valor, naipe):
+    def __init__(self, valor, naipe):
         self.valor = valor
         self.naipe = naipe
 
-    def _str_(self):
+    def __str__(self):
         return f"{self.valor} de {self.naipe}"
